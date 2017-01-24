c4d2a77082d30e957812b98af2309a21e26023fd
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-01-24 12:35:57 +0200

    Oops

--- scripts/circle-ci-tests.sh
+++ scripts/circle-ci-tests.sh
@@ -97,4 +97,4 @@ EXIT=0
 
 #cp -R $HOME/firefox/ firefox/
 #export SELENIUM_FIREFOX_PATH=firefox/firefox
-paver test_bokchoy --attr='shard_1' -n $NUMBER_OF_BOKCHOY_THREADS --with-flaky --with-xunit -t lms/test_lms:CourseWikiTest.test_edit_course_wiki
+paver test_bokchoy --attr='shard_1' -n $NUMBER_OF_BOKCHOY_THREADS --with-flaky --with-xunit -t lms/test_lms.py:CourseWikiTest.test_edit_course_wiki

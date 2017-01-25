324b8f155bd351fb0c60b0d4b2442a3b3e2dad39
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-01-25 14:46:47 +0200

    Run shard1 tests

--- scripts/circle-ci-tests.sh
+++ scripts/circle-ci-tests.sh
@@ -110,4 +110,4 @@ export SELENIUM_HOST=$SAUCE_USER_NAME:$SAUCE_API_KEY@ondemand.saucelabs.com
 #cp -R $HOME/firefox/ firefox/
 #export SELENIUM_FIREFOX_PATH=firefox/firefox
 
-paver test_bokchoy --attr='shard_1' -n $NUMBER_OF_BOKCHOY_THREADS --with-flaky --with-xunit -t lms/test_lms.py:CourseWikiTest.test_edit_course_wiki
+paver test_bokchoy --attr='shard_1' -n $NUMBER_OF_BOKCHOY_THREADS --with-flaky --with-xunit
